855ae1237bc4ce033b6991cf1956e1b3c3042acf
iantato <margiehouses@gmail.com>
2025-07-17 15:48:16 +0800

    Added constants for the files/data that we need.

---
+++ src/palgen/constants.py
@@ -0,0 +1,13 @@
+# IMPORTANT: These are game data constants. Keep in mind that
+# they should be in json format, not uasset format. If having
+# issues with getting the json format, use _FModel_ to extract
+# the data into json from the uasset files.
+
+"""Pal/Content/L10N/en/Pal/DataTable/Text/DT_PalNameText_Common.uasset"""
+PAL_NAME = "DT_PalNameText_Common.json"
+
+"""Pal/Content/Pal/DataTable/Character/DT_PalMonsterParameter.uasset"""
+PAL_INFO = "DT_PalMonsterParameter.json"
+
+"""Pal/Content/Pal/DataTable/Character/DT_PalCombiUnique.uasset"""
+UNIQUE_BREEDING = "DT_PalCombiUnique.json"
